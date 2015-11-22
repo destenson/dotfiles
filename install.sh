b124d937c18db5c2e8971b0253f36f3b65fc94bf
Dennis <destenson@gmail.com>
2015-11-22 02:44:57 -0800

    Fixed accuracy of comment

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 
-# Get current dir (so run this script from anywhere)
+# Get current dir (so this script can run from anywhere)
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 EXTRA_DIR="$HOME/.extra"
