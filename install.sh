82b735ddf971703d021bc913f8a195b459c0f1f3
Dennis <destenson@gmail.com>
2015-11-22 03:28:26 -0800

    updated global gitignore filename

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ grep "bash/_profile" ~/.bash_profile &> /dev/null || { echo; echo ". $DOTFILES_D
 
 ln -sfv "$DOTFILES_DIR/git/global.gitconfig" ~/.gitconfig
 #TODO: copy contents of files in gitignore project intelligently
-cp "$DOTFILES_DIR/git/.gitignore_global" ~/.gitignore
+cp "$DOTFILES_DIR/git/global.gitignore" ~/.gitignore
 if [ -d ~/gitignore/Global ]; then
   for ig in ~/gitignore/Global/*.gitignore ; do
 	cat $ig >> ~/.gitignore
